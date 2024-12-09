75761e4ef69fd5781138b6ed99fec395412c2ebe
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-12-09 18:46:00 -0300

    Create method to write lines of data
    I`ve decided to store the data on a CSV file as it is portable and
    generally lightweight compared to other formats. This method take a line
    od data and writes it to a CSV file.
    
    Moreover, another method to sanke case the platform name was created to
    standardize the platform names on the file.

--- service/data_validator.py
+++ service/data_validator.py
@@ -13,4 +13,11 @@ class DataValidator:
 
     def get_platform(self):
         if len(self.data) > 0:
-            self.platform = self.data[0]["platform"].replace(" ", "")
+            self.platform = self.data[0]["platform"]
+            
+    def snake_case_platform(self):
+        return self.platform.lower().replace(" ", "_")
+
+    def print_file_content(self, line):
+        with open(f"output/{self.time.timestamp}_{self.client}_{self.snake_case_platform()}.csv", 'a', encoding='utf-8') as file:
+            file.write(line)
