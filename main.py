15c223799594ad1ee1953339803fbf8b5dfc188a
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-12-10 15:35:04 -0300

    Turn the app into an API
    I know you're probably disgusted on a certain level to see these lots of
    comment lines. I know that feeling.
    
    As I'm trying not to lose track of the implemented stuff while turning
    it all into an API, I've decided to keep these comment lines.
    
    Just rest assured they won't be there for long

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import sys
 
+from fastapi import FastAPI
 from infrastucture.exceptions.non_existing_platform import NonExistingPlatformException
 from infrastucture.setup_verification import Setup
 from service.data_validator import DataValidator
 
-async def main():
-    lines_to_test, file_name = Setup.initialize_process(sys.argv)
-            
-    print("Number of lines to test: ", len(lines_to_test))
-    
-    try:
-        await DataValidator(lines_to_test, file_name).test_runner()
-    except NonExistingPlatformException as e:
-        print(str(e))
-    
+app = FastAPI()
 
-if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
-
+@app.get("/")
+async def root():
+    return { "message": "Hello World" }
+#     lines_to_test, file_name = Setup.initialize_process(sys.argv)
+#
+#     print("Number of lines to test: ", len(lines_to_test))
+#
+#     try:
+#         await DataValidator(lines_to_test, file_name).test_runner()
+#     except NonExistingPlatformException as e:
+#         print(str(e))
+#
+#
+# if __name__ == "__main__":
+#     import asyncio
+#     asyncio.run(main())
+#
 
 # for line in lines_to_test:
 #     print(line)
