1322a2bf4e3f3ce5e683b2c223b041004feb58fb
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-12-10 14:25:26 -0300

    Implement a method to test the preselected products

--- service/data_validator.py
+++ service/data_validator.py
@@ -24,3 +24,19 @@ class DataValidator:
     def print_file_content(self, line):
         with open(f"output/{self.time.strftime('%d_%m_%Y_%H_%M')}_{self.client}_{self.snake_case_platform()}.csv", 'a', encoding='utf-8') as file:
             file.write(line)
+            
+    async def test_runner(self):
+        self.print_file_content(
+            "Ean\tProdName\tTestProdName\tNameMatch\tPrice\tTestPrice\tPriceMatch\tBothMatch\tLinkUrl\n")
+        for product in self.data:
+            result = await self.platform.request_content(product["link"])
+            if "error" in result:
+                continue
+            ean = product["ean"]
+            expected_price = product["price_credit_card"]
+            expected_name = product["product_name"]
+            name_match = result["title"] == expected_name
+            price_match = result["price"] == expected_price
+            both_match = name_match and price_match
+            line = f"{ean}\t{expected_name}\t{result['title']}\t{name_match}\t{expected_price}\t{result['price']}\t{price_match}\t{both_match}\t{product['link']}\n"
+            self.print_file_content(line)
