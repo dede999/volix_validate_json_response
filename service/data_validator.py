906fb47126ba319766bdd3951e399c289ae764cd
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-12-10 14:22:08 -0300

    Add a platform to the data validator

--- service/data_validator.py
+++ service/data_validator.py
@@ -1,23 +1,26 @@
 
 from datetime import datetime
 
+from infrastucture.platform_factory import platform_factory
+
 
 class DataValidator:
     def __init__(self, data, file_name):
         self.time = datetime.now()
         self.data = data
         self.client = file_name.split("_")[0].split("/")[-1]
-        self.platform = ""
+        self.platform_name = ""
         self.get_platform()
-        print(f"DataValidator initialized at {self.time} for {self.client} on {self.platform}")
+        self.platform = platform_factory(self.platform_name)
+        print(f"DataValidator initialized at {self.time.strftime('%d_%m_%Y')} for {self.client} on {self.platform_name}")
 
     def get_platform(self):
         if len(self.data) > 0:
-            self.platform = self.data[0]["platform"]
+            self.platform_name = self.data[0]["platform"]
             
     def snake_case_platform(self):
-        return self.platform.lower().replace(" ", "_")
+        return self.platform_name.lower().replace(" ", "_")
 
     def print_file_content(self, line):
-        with open(f"output/{self.time.timestamp}_{self.client}_{self.snake_case_platform()}.csv", 'a', encoding='utf-8') as file:
+        with open(f"output/{self.time.strftime('%d_%m_%Y_%H_%M')}_{self.client}_{self.snake_case_platform()}.csv", 'a', encoding='utf-8') as file:
             file.write(line)
