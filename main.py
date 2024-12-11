d19fc11a4a1df39d8e9444606b7defa30c582718
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-12-11 12:21:31 -0300

    Complete `/validate` endpoint implementation

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-import sys
 from typing import Optional
 
-from fastapi import FastAPI, UploadFile
-from pydantic import BaseModel
+from fastapi import FastAPI, UploadFile, status, Response
 
 from infrastucture.exceptions.non_existing_platform import NonExistingPlatformException
 from infrastucture.setup_verification import Setup
@@ -15,21 +12,14 @@ app = FastAPI()
 async def root():
     return { "message": "Hello World" }
 
-@app.post("/validate")
-async def validate_map_data(file: UploadFile, lines: Optional[int] = 20):
+@app.post("/validate", status_code=status.HTTP_200_OK)
+async def validate_map_data(file: UploadFile, response: Response, lines: Optional[int] = 20):
     lines_to_test, file_name = Setup.initialize_process(lines, file)
-    return { "lines": lines_to_test, "file": file_name, "lines_to_test": lines_to_test }
-#     try:
-#         await DataValidator(lines_to_test, file_name).test_runner()
-#     except NonExistingPlatformException as e:
-#         print(str(e))
-#
-#
-# if __name__ == "__main__":
-#     import asyncio
-#     asyncio.run(main())
-#
 
-# for line in lines_to_test:
-#     print(line)
-#     print("\n")
+    try:
+        result = await DataValidator(lines_to_test, file_name).test_runner()
+        return { "result": result }
+
+    except NonExistingPlatformException as e:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return { "result": str(e) }
