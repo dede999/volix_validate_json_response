a8ecfaf0a5137f0169f688717dd5e02a4f008f82
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-12-12 10:42:43 -0300

    Improve request response for ghost platforms
    The term "ghost platform" was designed to refer to the platform that
    does not exist in the system. "Ghost" is shorter than "non-existent",
    and the last one might not fit on my self-imposed limit of 55 characters
    in the commit subject limit.
    
    Keeping the commit subject restricted is considered a good practice due
    to the fact that longer commit titles may overflow from some screens,
    and reduce visibility.
    
    About the commit itself, it seeks to improve the response message from
    "There is no such platform as SitePlatform" to "There is no implemented
    platform for Site (SitePlatform)".
    
    Given this software is mean for internal usage, in the next commit I'll
    include a tip that may help solving this problem

--- infrastucture/exceptions/non_existing_platform.py
+++ infrastucture/exceptions/non_existing_platform.py
@@ -1,4 +1,4 @@
 
 class NonExistingPlatformException(Exception):
     def __init__(self, searched_platform: str):
-        super().__init__(f"There is no such platform as {searched_platform}")
+        super().__init__(f"There is no implemented platform for {searched_platform}")
--- infrastucture/platform_factory.py
+++ infrastucture/platform_factory.py
@@ -9,4 +9,4 @@ def platform_factory(platform: str) -> BasePlatform:
     try:
         return globals()[platform_name]()
     except KeyError:
-        raise NonExistingPlatformException(platform_name)
+        raise NonExistingPlatformException(f"{platform} ({platform_name})")
