ba1c4d830c24f60ec0cdecfa2a2279a327328489
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-12-13 19:21:07 -0300

    Make Magalu platform use selenium platform

--- domain/platforms/magazine_luiza_platform.py
+++ domain/platforms/magazine_luiza_platform.py
@@ -1,50 +1,16 @@
-from webdriver_manager.firefox import GeckoDriverManager
+import re
 
-from domain.platforms.base_platform import BasePlatform
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+from domain.platforms.selenium_platform import SeleniumPlatform
 
+class MagazineLuizaPlatform(SeleniumPlatform):
+    def get_price(self) -> float:
+        price_element = self.driver_wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "p[data-testid='price-value']")))
+        price_text = price_element.text.replace("R$", "").replace(".", "").replace(",", ".")
+        return float(re.sub(r"[a-zA-Z]", "", price_text))
 
-class MagazineLuizaPlatform(BasePlatform):
-    def __init__(self):
-        self.soup = None
-        print("MagazineLuizaPlatform initialized")
-
-    def get_headers(self) -> dict[str, str]:
-        return {}
-
-    def get_cookies(self) -> dict[str, str]:
-        return {}
-
-    async def request_content(self, url: str):
-        # Configurações do Firefox para evitar detecção de bot
-        firefox_options = Options()  # Use Firefox Options
-        firefox_options.add_argument("--headless")
-        firefox_options.set_preference("general.useragent.override", self.create_random_user_agent())
-
-        # Inicializar o WebDriver do Firefox
-        service = Service(GeckoDriverManager().install())
-        driver = webdriver.Firefox(service=service, options=firefox_options)
-
-        try:
-            driver.get(url)
-
-            # Resto do código permanece igual
-            wait = WebDriverWait(driver, 10)
-            product_name = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "h1"))).text
-            price_element = wait.until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "p[data-testid='price-value']")))
-            price = price_element.text
-
-            driver.quit()
-            return {"title": product_name, "price": price}
-
-        except Exception as e:
-            print(f"Erro durante o scraping: {e}")
-            driver.quit()
-            return {"error": str(e)}
+    def get_title(self) -> str:
+        title_element = self.driver_wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "h1")))
+        return title_element.text
