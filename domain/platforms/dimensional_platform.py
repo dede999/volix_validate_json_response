9815b9e57272bc742e7ca5bb9767dcb61ae85b85
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-12-24 12:12:57 -0300

    Implement platform for dimensional validation
    It works exactly the same way as the andra platform. Mostly because the
    CSS classes used across these sites are the same, I was able to copy the
    code in the "AndraPlatform" onto the "DimensionalPlatform".
    
    The next commits will bring a solution for the duplicated code

---
+++ domain/platforms/dimensional_platform.py
@@ -0,0 +1,20 @@
+from domain.platforms.andra_dimensional_strategy import AndraDimensionalStrategy
+
+
+class DimensionalPlatform(AndraDimensionalStrategy):
+    def get_title(self) -> str:
+        return self.soup.find('h1').text
+
+    def get_whole_price(self) -> str:
+        return self.soup.find('span', class_='vtex-product-price-1-x-currencyInteger').text
+
+    def get_fraction_price(self) -> str:
+        return self.soup.find('span', class_='vtex-product-price-1-x-currencyFraction').text
+
+    def get_price(self) -> float:
+        try:
+            price_whole = self.get_whole_price()
+            price_fraction = self.get_fraction_price()
+            return float(f"{price_whole}.{price_fraction}")
+        except AttributeError:
+            return 0.0
